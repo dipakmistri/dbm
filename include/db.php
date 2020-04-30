73d287e648a20e9f306a695fb4ca0e2fe2948b9c
Desai Ramesh <desairamesh955@gmail.com>
2020-04-30 15:10:54 +0530

    first commit

---
+++ include/db.php
@@ -0,0 +1,28 @@
+<?php
+
+ class DB{
+    public $con;
+    
+    public function connect(){
+        //                             sql server name           databae name
+        define('SERVER',"sqlsrv:Server=DESKTOP-AI1N279\SQLEXPRESS;Database=HRMS");
+        define('USERNAME','sa');//username
+        define('PASSWORD','1234');//passweord
+        
+        try{
+            $this->con = new PDO(SERVER, USERNAME, PASSWORD);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if($this->con){
+                return $this->con;//return database connection
+            }
+        }
+        catch(PDOException $ex){
+            die("connection failed");
+        }
+    }
+ 
+ }
+
+//  $db = new DB();
+// $db->connect();    //database object
+?>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+creatae database name HRMS
+copy scripts from HRMS.sql
+execute these lines in HRMS databasea
